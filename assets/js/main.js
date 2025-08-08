af5573528f97d11c7b88ffd8f5d332cac28c2c5a
Francisco Godoy <fgodoyr93@gmail.com>
2025-08-07 23:08:57 -0400

    pruebas con realizar pedido

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,15 @@
 //1. Crear funciones que devuelven promesas.
 
-const tiempoCafe = (Math.random()*(3-1)+1)*1000
-const tiempoPan = (Math.random()*(4-2)+2)*1000
-const tiempoJugo = (Math.random()*(2-1)+1)*1000
+const tiempoCafe = (Math.random()*(3-1)+1).toFixed(2)*1000
+const tiempoPan = (Math.random()*(4-2)+2).toFixed(2)*1000
+const tiempoJugo = (Math.random()*(2-1)+1).toFixed(2)*1000
 
 const prepararCafe = () => {
     return new Promise((resolve, reject) => {
     setTimeout(() => {
         const exito = Math.random() > 0.2
         if(exito){
-            console.log("El café está listo")
+            console.log(`El café está listo`, `en ${tiempoCafe/1000} segundos`)
         }else{
             reject("Se acabó el café")
         }}, tiempoCafe)
@@ -21,7 +21,7 @@ const tostarPan = () => {
     setTimeout(() => {
         const exito = Math.random() > 0.2
         if(exito){
-            console.log("Pan tostado listo")
+            console.log(`Pan tostado listo en ${tiempoPan/1000} segundos`)
         }else{
             reject("Se acabó el pan")
         }}, tiempoPan)
@@ -33,16 +33,58 @@ const exprimirJugo = () => {
     setTimeout(() => {
         const exito = Math.random() > 0.2
         if(exito){
-            console.log("Jugo listo")
-        }else{
+            console.log(`Jugo listo en ${tiempoJugo/1000} segundos`)
+            }else{
             reject("No queda fruta para el jugo")
-        }}, tiempoPan)
+        }}, tiempoJugo)
     })
 }
 
-prepararCafe()
-tostarPan()
-exprimirJugo()
+// prepararCafe()
+// tostarPan()
+// exprimirJugo()
 
 //2. Realizar el pedido
+// const realizarPedido = () => {
+//     console.log("Realizando pedido...")
+//     Promise.all([prepararCafe(), tostarPan(), exprimirJugo()])
+//     .then( () => {
+//         console.log("Pedido listo, buen provecho")
+//     })
+//     .catch( (error) => {
+//         console.log(`No se pudo completar el pedido: ${error}`)
+//     })
+// }
 
+// const realizarPedido = () => {
+//     console.log("Realizando pedido...")
+
+//     prepararCafe()
+//     .then( () => {
+//         console.log("Café preparado")
+//         // tostarPan()
+//     })
+//     .catch( (error) => {
+//         console.log(`No se pudo preparar el café: ${error}`)
+//         return tostarPan() // Si falla el café, intentamos tostar el pan
+//     }) //hasta aca funciona
+//     .then( () => tostarPan() )
+//         .then( () => {
+//             console.log("Pan preparado")
+//             // exprimirJugo()
+//         })
+    
+//     .catch( (error) => {
+//         console.log(`No se pudo preparar el pan: ${error}`)
+//     }) //hasta aca funciona
+//     .then( () => {
+//         console.log("Jugo exprimido")
+//     })
+//     .catch( (error) => {
+//         console.log(`No se pudo preparar el jugo: ${error}`)
+//     })
+//     .then( () => {
+//         console.log("Pedido listo, buen provecho")
+//     })
+// }
+realizarPedido()
